57442ff14801264074bde6ca5218fabfb1509189
rutu <rutugaikwad843@gmail.com>
2021-01-23 15:46:06 +0530

    Before Homework

---
+++ src/main/java/com/upgrad/mba/entities/Language.java
@@ -0,0 +1,42 @@
+package com.upgrad.mba.entities;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+
+@Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Language {
+    @Id
+    @GeneratedValue
+    private int languageId;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String languageName;
+
+    public int getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(int languageId) {
+        this.languageId = languageId;
+    }
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
+
+    @Override
+    public String toString() {
+        return "Language{" +
+                "languageId=" + languageId +
+                ", languageName='" + languageName + '\'' +
+                '}';
+    }
+}
